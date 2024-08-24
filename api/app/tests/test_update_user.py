af2c61aceb5d5b1ea0b55945df9cbb310a7127de
Aleksander Okoń <43412672+aleksander124@users.noreply.github.com>
2024-08-24 22:57:06 +0200

    check user_update with invalid email or not existing user

--- api/app/tests/test_update_user.py
+++ api/app/tests/test_update_user.py
@@ -1,7 +1,7 @@
 from fastapi.testclient import TestClient
 
 
-def test_update_user(client: TestClient):
+def create_user(client: TestClient):
     user_data = {
         "username": "originaluser",
         "email": "original@test.com",
@@ -13,7 +13,11 @@ def test_update_user(client: TestClient):
     response = client.post("/auth/create-user/", json=user_data)
     assert response.status_code == 200
     created_user = response.json()
-    user_id = created_user["id"]
+    return created_user["id"], user_data
+
+
+def test_update_user(client: TestClient):
+    user_id, original_user_data = create_user(client)
 
     update_data = {
         "username": "updateduser",
@@ -28,5 +32,30 @@ def test_update_user(client: TestClient):
     assert updated_user["id"] == user_id
     assert updated_user["username"] == update_data["username"]
     assert updated_user["email"] == update_data["email"]
-    assert updated_user["is_active"] == user_data["is_active"]
-    assert updated_user["is_superuser"] == user_data["is_superuser"]
+    assert updated_user["is_active"] == original_user_data["is_active"]
+    assert updated_user["is_superuser"] == original_user_data["is_superuser"]
+
+
+def test_update_user_invalid_email(client: TestClient):
+    user_id, original_user_data = create_user(client)
+
+    update_data = {
+        "username": "updateduser",
+        "email": "invalidemail",
+    }
+
+    response = client.put(f"/auth/users/{user_id}/", json=update_data)
+    assert response.status_code == 400
+    assert response.json() == {"detail": "Invalid email address"}
+
+
+def test_update_user_not_exists(client: TestClient):
+    update_data = {
+        "username": "originaluser",
+        "email": "original@test.com",
+    }
+
+    invalid_user_id = 99
+    response = client.put(f"/auth/users/{invalid_user_id}/", json=update_data)
+    assert response.status_code == 404
+    assert response.json() == {"detail": "User not found"}
